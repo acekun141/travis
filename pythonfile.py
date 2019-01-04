f43166953059313a2ce4577b277f451094d2eec1
unknown <acekun141@gmail.com>
2019-01-04 19:12:32 +0700

    Fix pythonfile.py

--- pythonfile.py
+++ pythonfile.py
@@ -3,7 +3,7 @@ def is_prime(n):
 	"""Xác định số nguyên tố."""
 	if n < 2:
 		return False
-	for i in range(2,int(math.sqrt(n))):
+	for i in range(2,int(math.sqrt(n))+1):
 		if n % i == 0:
 			return False
 	return True
